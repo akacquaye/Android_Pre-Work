cf9926f32848d6c75a8a79873434a223b9332775
Alfred K. Acquaye <31114845+akacquaye@users.noreply.github.com>
2018-10-01 22:53:16 -0400

    This is my submission.

---
+++ src/main/java/com/codepath/mobileappdesign_androidpre_workproject/MainActivity.java
@@ -0,0 +1,23 @@
+package com.codepath.mobileappdesign_androidpre_workproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+    }
+}
